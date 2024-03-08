4fe2acd56e80a5559c44b7951d5dd49acd8fa887
ashishbhoure <ashishbhoure@gmail.com>
2024-03-08 11:18:27 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,20 @@ import "./index.css";
 import Navbar from "./components/Navbar";
 import { Home, About, Projects, Contact, Resume } from "./pages";
 import Footer from "./components/Footer";
+import { Helmet } from "react-helmet";
 // import Resume from "./pages/Resume";
 
 const App = () => {
   return (
     <Router>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Ashish Bhoure | Portfolio</title>
+        <meta
+          name="description"
+          content="Ashish Bhoure's portfolio showcases innovative software solutions crafted with precision and creativity."
+        />
+      </Helmet>
       <Navbar />
       <main className="bg-slate-300/20 h-[100%] scroll-smooth">
         <Routes>
