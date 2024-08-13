3e038b18760418bfbb02936d0d2a5eb1e07cf95b
ashishbhoure <ashishbhoure@gmail.com>
2024-08-13 11:02:54 +0530

    follow button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,6 +22,14 @@ const Footer = () => {
               />
             </Link>
           ))}
+          <a
+            href="https://www.linkedin.com/comm/mynetwork/discovery-see-all?usecase=PEOPLE_FOLLOWS&followMember=ashishbhoure"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex flex-col justify-center items-center px-4 py-2 text-center text-white w-48 h-8 rounded-full bg-[#0A66C2] font-sans"
+          >
+            Follow on LinkedIn
+          </a>
         </div>
       </div>
     </footer>
